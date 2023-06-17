dda073c0d543dd863105ccc786de130b76cff64f
yoeltripp <cris.ct2003@gmail.com>
2023-06-17 18:08:49 -0400

    Create connector.py

---
+++ connector.py
@@ -0,0 +1,18 @@
+import pymysql
+from main import Usuario
+cone = pymysql.connect( host='localhost', user= 'root', passwd='admin', db='neoguias' )
+cur = cone.cursor()
+
+
+
+class Operaciones:
+    def getInfoUsuario(self):
+        cur.execute( "SELECT * FROM cliente" )
+        lista = []
+        for rut, nombre, contra, fnac, dir, pais, reserva in cur:
+            op1 = Usuario(rut, nombre, contra, fnac, dir, pais, reserva)
+            lista.append(op1)
+        cur.close()
+        cone.close()
+        return lista
+    # 
