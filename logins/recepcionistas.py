dd8559baea64616e82232184959797206c0767a9
yoeltripp <cris.ct2003@gmail.com>
2023-06-18 18:23:07 -0400

    Update recepcionistas.py

--- logins/recepcionistas.py
+++ logins/recepcionistas.py
@@ -1,4 +1,3 @@
-# Registrar a los recepcionistas
 from principal.main import bconsola
 from principal.connector import Operaciones
 import time
@@ -7,43 +6,36 @@ con = Operaciones
 listarecepcionistas = []
 
 
-def registrar_recepcionista():
-    mail = input("Ingrese el correo que desea utilizar: ")
-    contra = input("Ingrese la contraseña: ")
-    contrac = input("Confirme la contraseña: ")
-    
-    listarecepcionistas = con.getInfoTrabajador()
-    
-    if mail != user.correo:
-        if contra == contrac:
-            print("Te has registrado correctamente, te moveremos al panel de Hostales")
-            con.crearRecepcionista("SOLOUSER",mail, contra)
-
-            time.sleep(2)
-            bconsola()
-            return
-        else:
-                print("Contraseñas incorrectas")
-                registrar_recepcionista()
-                return  # Salir del bucle for cuando la contraseña sea incorrecta
-    else:
-             print("Este correo ya ha sido utilizado, porfavor intente con otro")
-             return 
-         
-    
+            
     # Login de los recepcionistas 
     
 def login_recepcionista():
-    correo = input("Ingrese su correo: ")
-    contra = input("Ingrese su contraseña: ")
+    intentos = 0
+    listarecepcionistas = con.getInfoTrabajador()
+    bconsola()
+    print("=============== Inicie Sesión como Colaborador ================")
+    while intentos < 3:
+        correo = input("Ingrese su correo: ")
+        cval = False
+        for r in listarecepcionistas:
+            if correo == r.mail:
+                cval = True
+                contra = input("Ingrese su contraseña: ")
+                if contra == r.contrasena:
+                    print("Bienvenido/a", r.nombre, r.apellido)
+                    print("Te redirigiremos al panel de Trabajador")
+                    time.sleep(3)
+                    bconsola()
+                    # funcion menu trabajador
+                    return
+                else:
+                     print("Contraseña incorrecta")
+                     break
+        intentos += 1
+        if not cval:
+            print("Este correo no es válido")
+
+    print("\n===============Has alcanzado el límite de intentos.===============")
+    time.sleep(2)
+    login_recepcionista()            
     
-    listarecepcionistas = con.getInfoRecepcionista()
-    for recepcionista in listarecepcionistas:
-        if correo == recepcionista.correo and contra == recepcionista.contraseña:
-            print("Inicio de sesión exitoso como recepcionista")
-            
-            # llevar al menu principal (no se como se hace xD)
-            
-            return
-    print("Credenciales incorrectas. Intente nuevamente.")
-    login_recepcionista()
