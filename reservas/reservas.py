ac6ee91271fcfc147d463e1dd3f98336e1abcd0f
yoeltripp <cris.ct2003@gmail.com>
2023-06-19 00:34:35 -0400

    Update reservas.py

--- reservas/reservas.py
+++ reservas/reservas.py
@@ -7,29 +7,15 @@ def registrar_reserva(nombre, fecha, metodoPago):
     
 # Mostrar las reservas concretadas
 
-def mostrar_reservas(self):
+def mostrar_reservas():
 
-    if not reservas:
-        print("No hay reservas registradas")
-    else:
-        for reserva in reservas:
-            print("Nombre:", reserva[0])
-            print("Fecha:", reserva[1])
-            print("Metodo de Pago:", reserva[2])
-            print()  
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Re")
 
-    conn.close()
+    
 
 
-# inicar sesion como trabajador
-
-def iniciar_sesion_recepcionista(usuario, contraseña):
 
 
-    if resultado:
-        print("Inicio de sesión exitoso como trabajador")
-        
-    else:
-        print("Credenciales incorrectas, inicio de sesión fallido")
 
-    conn.close()
+
