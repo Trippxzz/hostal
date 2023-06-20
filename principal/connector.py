b0cad0df2dae4f18574241ab57e10e098c66f30a
yoeltripp <cris.ct2003@gmail.com>
2023-06-19 22:32:44 -0400

    Update connector.py

--- principal/connector.py
+++ principal/connector.py
@@ -8,20 +8,32 @@ class Operaciones:
         cur = cone.cursor()
         cur.execute( "SELECT * FROM cliente" )
         lista = []
-        for rut, nombre, correo, contra, fnac, dir, pais in cur:
-            op1 = Usuario(rut, nombre, correo,contra, fnac, dir, pais)
+        for rut, nombre, correo, contra, fnac, dir, pais, ntelefono in cur:
+            op1 = Usuario(rut, nombre, correo,contra, fnac, dir, pais, ntelefono)
             lista.append(op1)
         cur.close()
         cone.close()
         return lista
+    
+    def getCorreoRegister(cor):
+        cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
+        cur = cone.cursor()
+        cur.execute("""SELECT * FROM cliente WHERE Correo = %s """,(cor,))
+        lista = []
+        for rut, nombre, correo, contra, fnac, dir, pais, ntelefono in cur:
+            op6 = Usuario(rut, nombre, correo, contra, fnac, dir, pais, ntelefono)
+            lista.append(op6)
+        cur.close()
+        cone.close()
+        return lista 
     
     def getRut(cor):
         cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
         cur = cone.cursor()
         cur.execute("""SELECT * FROM cliente WHERE Correo = %s """,(cor,))
         lista = []
-        for rut, nombre, correo, contra, fnac, dir, pais in cur:
-            op6 = Usuario(rut, nombre, correo, contra, fnac, dir, pais)
+        for rut, nombre, correo, contra, fnac, dir, pais, ntelefono in cur:
+            op6 = Usuario(rut, nombre, correo, contra, fnac, dir, pais, ntelefono)
             lista.append(op6)
         cur.close()
         cone.close()
@@ -38,6 +50,18 @@ class Operaciones:
         cur.close()
         cone.close()
         return lista
+    
+    def getInfoTrabajadorC(c):
+        cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
+        cur = cone.cursor()
+        cur.execute( "SELECT * FROM Recepcionista WHERE Gmail = %s """, (c,) )
+        lista = []
+        for rut, nombre, apellido, contra, horario, sueldo, mail, tel, idhostal in cur:
+            op2 = Recepcionista(rut, nombre, apellido, contra, horario, sueldo, mail, tel, idhostal)
+            lista.append(op2)
+        cur.close()
+        cone.close()
+        return lista
         
     def crearUsuario(rut, correo, contra):
         cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
@@ -47,23 +71,35 @@ class Operaciones:
         cur.close()
         cone.close()
         
-    def actualizar_a_cliente(rut, nombre, correo, contra, fnac, dire, p):
-        cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
+    def actualizar_a_cliente(rut, nombre, correo, contra, fnac, dire, p, ntelefno):
+        cone = pymysql.connect(host='localhost', user='root', passwd='', db='hostal')
         cur = cone.cursor()
-        cur.execute("""UPDATE  cliente SET (RUT, Correo, Contrasena, Nacimiento, Direccion, Pais ) VALUES (%s,%s, %s, %s,%s, %s, %s) """,(rut, nombre, correo, contra, fnac, dire, p))
-        cone.commit()
+        sql = """UPDATE cliente SET RUT = %s, Nombre = %s, Contrasena = %s, Nacimiento = %s, Direccion = %s, Pais = %s, NTelefono = %s WHERE Correo = %s"""
+        values = (rut, nombre, contra, fnac, dire, p,ntelefno, correo)
+        cur.execute(sql, values)
+        cone.commit()        
         cur.close()
-        cone.close()       
+        cone.close()
+
     
     def crear_reservas(IdReserva, IdHostal, IdHabitacion, IdHuesped, IdRecepcionista, FechaLlegada, FechaSalida):
         cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
         cur = cone.cursor()
-        cur.execute("""INSERT INTO reservas (IdReserva, IdHostal, IdHabitacion, IdHuesped, IdRecepcionista, FechaLlegada, FechaSalida) VALUES (%s,%s, %s,%s,%s, %s,%s)
+        cur.execute("""INSERT INTO reserva (IdReserva, IdHostal, IdHabitacion, IdHuesped, IdRecepcionista, FechaLlegada, FechaSalida) VALUES (%s,%s, %s,%s,%s, %s,%s)
                    """,(IdReserva, IdHostal, IdHabitacion, IdHuesped, IdRecepcionista, FechaLlegada, FechaSalida))
         cone.commit()
         cur.close()
         cone.close()
-        
+
+    def actualizar_disponibilidad(canthab, disponibilidad, idh):
+        cone = pymysql.connect(host='localhost', user='root', passwd='', db='hostal')
+        cur = cone.cursor()  
+        sql = """UPDATE habitacion SET MaxHab = %s, Disponibilidad = %s WHERE IdHostal = %s"""
+        values = (canthab, disponibilidad, idh)    
+        cur.execute(sql, values)
+        cone.commit()
+        cur.close()
+        cone.close()          
 
     def mostrar_reservas():       
         cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
@@ -100,3 +136,12 @@ class Operaciones:
         cur.close()
         cone.close()
         return lista
+
+    def genregPago(ide,monto,mpago ,factual):
+        cone = pymysql.connect( host='localhost', user= 'root', passwd='', db='hostal' )
+        cur = cone.cursor()
+        cur.execute("""INSERT INTO pago (Ntransaccion, Monto, Metodo, FechaDePago) VALUES (%s,%s, %s,%s)
+                   """,(ide,monto,mpago ,factual))
+        cone.commit()
+        cur.close()
+        cone.close()
